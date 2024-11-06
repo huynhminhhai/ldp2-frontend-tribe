7de110662e0690f165758c705d99526c8af2a924
Huynh Minh Hai <huynhminhhai.1555@gmail.com>
2024-11-06 13:54:22 +0700

    build logo ticker section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -4,8 +4,10 @@ import Image from "next/image";
 export const Header = () => {
   return (
     <header className="py-4 border-b border-white/15 md:border-none sticky top-0 z-10">
+      <div className="absolute inset-0 backdrop-blur -z-10 md:hidden"></div>
       <div className="container">
-        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto backdrop-blur">
+        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto relative">
+          <div className="absolute inset-0 backdrop-blur -z-10 hidden md:block"></div>
           <div>
             <div className="border h-10 w-10 rounded-lg inline-flex justify-center items-center border-white/15">
               <Image src='/logo.svg' alt="logo" width={300} height={300} className="h-8 w-8 image-white" />
--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,25 @@
+import Image from "next/image";
+
 export const LogoTicker = () => {
-  return <section>LogoTicker</section>;
+  return (
+    <section className="py-20 md:py-24">
+      <div className="container">
+        <div className="flex items-center gap-5">
+          <div className="flex-1 md:flex-none">
+            <h2>Trusted by top innovative teams</h2>
+          </div>
+          <div className="flex-1 overflow-hidden mask-gradient-hori-black">
+            <div className="flex flex-none gap-14">
+              <Image src='/logo-acme.png' alt="logo acme" width={300} height={300} className="h-6 w-auto" />
+              <Image src='/logo-apex.png' alt="logo acme" width={300} height={300} className="h-6 w-auto" />
+              <Image src='/logo-celestial.png' alt="logo acme" width={300} height={300} className="h-6 w-auto" />
+              <Image src='/logo-quantum.png' alt="logo acme" width={300} height={300} className="h-6 w-auto" />
+              <Image src='/logo-pulse.png' alt="logo acme" width={300} height={300} className="h-6 w-auto" />
+              <Image src='/logo-echo.png' alt="logo acme" width={300} height={300} className="h-6 w-auto" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
