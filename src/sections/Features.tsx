afcaeb634763ff2770cc82fc9e15a7064cb255c5
Huynh Minh Hai <huynhminhhai.1555@gmail.com>
2024-11-06 16:39:47 +0700

    build features section

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,3 +1,7 @@
+'use client'
+import {DotLottiePlayer} from '@dotlottie/react-player'
+import Image from 'next/image';
+
 const tabs = [
   {
     icon: "/assets/lottie/vroom.lottie",
@@ -26,5 +30,32 @@ const tabs = [
 ];
 
 export const Features = () => {
-  return <section>Features</section>;
+  return (
+    <section className="py-10 md:py-24">
+      <div className="container">
+        <h2 className='text-5xl leading-[1.2] md:text-6xl font-medium text-center tracking-tighter'>Elevate your SEO efforts.</h2>
+        <p className='text-white/70 text-lg md:text-xl max-w-2xl mx-auto tracking-tight text-center mt-5'>
+          From small srartup to large enterprises, our AI-driven tool has revolutionized the way businesses approach SEO.
+        </p>
+        <div className='mt-10 flex flex-col gap-3'>
+          {
+            tabs.map((tab, i) => (
+              <div key={i} className='border border-white/15 flex items-center p-2.5 rounded-xl gap-2.5 md:flex-1'>
+                <div className='h-12 w-12 border border-white/15 rounded-lg inline-flex items-center justify-center'>
+                  <DotLottiePlayer src={tab.icon} className='h-5 w-5' autoplay loop />
+                </div>
+                <div className='font-medium'>{tab.title}</div>
+                {tab.isNew && <div className='text-xs rounded-full px-2 py-0.5 bg-[#8c44ff] text-black font-semibold'>new</div>}
+              </div>
+            ))
+          }
+        </div>
+        <div className='border border-white/15 p-2.5 rounded-xl mt-3'>
+          <div className='aspect-video bg-cover border border-white/20 rounded-lg' style={{
+            backgroundImage: 'url(/product-image.png)'
+          }}></div>
+        </div>
+      </div>
+    </section>
+  );
 };
